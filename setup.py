331709e1217e79cd3246e271d1d356ceeec7491e
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-06 12:27:59 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+""" Setup to allow pip installs of pok-eco module """
+
+from setuptools import setup
+
+setup(
+    name='ascit_cert',
+    version='0.0.1',
+    description='ASICT CERT Integrations ',
+    author='METID - Politecnico di Milano',
+    url='http://www.metid.polimi.it',
+    license='AGPL',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    packages=['asictapi'],
+    dependency_links=[],
+    install_requires=[
+        "django==1.8.7",
+        "django-celery==3.1.16",
+        "tincan==0.0.5",
+        "pyoai==2.4.4"
+    ]
+)
