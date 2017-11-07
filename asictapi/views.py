fbbcce1bcf3c1e1839f34b28e274036c220073c4
Max <macsimbodnar@gmail.com>
2017-11-07 15:12:15 +0100

    messo l'import che mancava

--- asictapi/views.py
+++ asictapi/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from django.http import JsonResponse
 from django.db import connection
 
