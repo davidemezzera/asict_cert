091d3f2183c01c3e7cd10b8356e45928c6db9637
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-07 11:46:48 +0100

    adding start and end date

--- asictapi/views.py
+++ asictapi/views.py
@@ -18,12 +18,12 @@ def heartbeat(request):  # pylint: disable=unused-argument
     return JsonResponse(risposta)
 
 
-def certs_download(request, date_certs):  
+def certs_download(request, start_date, end_date):  
  
     with connection.cursor() as cursor:
         cursor.execute(
-            "SELECT SUBSTRING(uid,14) as CODICE_PERSONA, AP.name as NAME, course_id,CG.created_date as DATA_CERTIFICATO from certificates_generatedcertificate CG JOIN auth_user A on A.id=CG.user_id LEFT JOIN auth_userprofile AP on A.id=AP.user_id LEFT JOIN social_auth_usersocialauth SAU on A.id=SAU.user_id where CG.status = 'downloadable' and SUBSTRING(uid,14) IS NOT NULL and provider != 'ecoopenid-auth' AND CONVERT(CG.created_date, datetime) = CONVERT(%s, datetime)", 
-            [date_certs])
+            "SELECT SUBSTRING(uid,14) as CODICE_PERSONA, AP.name as NAME, course_id,CG.created_date as DATA_CERTIFICATO from certificates_generatedcertificate CG JOIN auth_user A on A.id=CG.user_id LEFT JOIN auth_userprofile AP on A.id=AP.user_id LEFT JOIN social_auth_usersocialauth SAU on A.id=SAU.user_id where CG.status = 'downloadable' and SUBSTRING(uid,14) IS NOT NULL and provider != 'ecoopenid-auth' AND CONVERT(CG.created_date, datetime) >= CONVERT(%s, datetime) AND CONVERT(CG.created_date, datetime) <= CONVERT(%s, datetime)", 
+            [start_date, end_date])
         result = cursor.fetchall()
 
     return JsonResponse(result, safe=False)
