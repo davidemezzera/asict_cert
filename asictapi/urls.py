72e334f8e5b75ef951bba70d75b4ac69b0aae936
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-08 17:27:40 +0100

    test authentication4

--- asictapi/urls.py
+++ asictapi/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls import patterns, url
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns(
     '',
-    url(r'^heartbeat', 'asictapi.views.heartbeat', name = 'asictapi_heartbeat'),
+    url(r'^heartbeat', CreateView.as_view(), name = 'asictapi_heartbeat'),
     url(r'^get_certs/last', 'asictapi.views.certs_download_yesterday', name = 'certs_download_yesterday'),
     url(r'^get_certs/(?P<start_date>[^/]+)/(?P<end_date>[^/]+)', 'asictapi.views.certs_download', name = 'asictapi_certs_download'),
 )
