54249b4a6048ab9818def7bbfcbd578e31e0b3a5
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-08 16:09:51 +0100

    test authentication3

--- asict_cert/settings.py
+++ asict_cert/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.OAuth2AuthenticationAllowInactiveUser',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
