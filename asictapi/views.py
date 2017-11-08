15239d5dc7e246cad53429dde0d58cc1964f17b7
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-08 15:16:33 +0100

    test superuser

--- asictapi/views.py
+++ asictapi/views.py
@@ -2,6 +2,7 @@
 from datetime import date, datetime, timedelta
 from django.http import JsonResponse
 from django.db import connection
+from rest_framework.exceptions import PermissionDenied
 import logging
 
 
@@ -19,6 +20,9 @@ def heartbeat(request):  # pylint: disable=unused-argument
 
 def certs_download(request, start_date, end_date):  
  
+    if !(request.user.is_superuser)
+        raise PermissionDenied() 
+
     with connection.cursor() as cursor:
         cursor.execute(
             "SELECT SUBSTRING(uid,14) AS CODICE_PERSONA, AP.name AS NAME, course_id, CG.created_date AS DATA_CERTIFICATO \
@@ -42,8 +46,6 @@ def certs_download_yesterday(request):
 
     yesterday = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
     today = date.today().strftime("%Y-%m-%d")
-    logging.warning('LOGGING LOGGING') 
     logging.warning(request.user.get_username())   
-
+    
     return certs_download(request, yesterday, today)    
-    #return JsonResponse(request, safe=False)
