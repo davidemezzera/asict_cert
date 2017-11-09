92501e642d55327a03efac60d1c3f9441a4aa77c
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-09 10:13:59 +0100

    test adding apiView

--- asictapi/urls.py
+++ asictapi/urls.py
@@ -3,13 +3,13 @@ URLs for mobile WEBAPI
 """
 from django.conf import settings
 from django.conf.urls import patterns, url
-from .views import CreateView
+from .views import AunicaCertView
 
 
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns(
     '',
-    url(r'^heartbeat', CreateView.as_view(), name = 'asictapi_heartbeat'),
+    url(r'^heartbeat', AunicaCertView.as_view(), name = 'asictapi_heartbeat'),
     #url(r'^get_certs/last', 'asictapi.views.certs_download_yesterday', name = 'certs_download_yesterday'),
     #url(r'^get_certs/(?P<start_date>[^/]+)/(?P<end_date>[^/]+)', 'asictapi.views.certs_download', name = 'asictapi_certs_download'),
 )
