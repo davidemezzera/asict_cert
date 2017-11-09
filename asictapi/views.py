8b790c81604f5e7c9b22d16dd458dcc6d0d381b8
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-09 10:57:24 +0100

    test adding testView

--- asictapi/views.py
+++ asictapi/views.py
@@ -15,7 +15,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from MyProject.MyApp import CalcClass
 
 
 class MyRESTView(APIView):
