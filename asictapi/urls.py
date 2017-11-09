8485d791658b39624d3df7bce8adbca420f4d700
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-09 15:08:25 +0100

    fix 2

--- asictapi/urls.py
+++ asictapi/urls.py
@@ -14,12 +14,12 @@ urlpatterns = patterns(
     #url(r'^get_certs/(?P<start_date>[^/]+)/(?P<end_date>[^/]+)', 'asictapi.views.certs_download', name = 'asictapi_certs_download'),
 )'''
 
-from .views import HeartRESTView, CertsRESTView
+from .views import HeartRESTView, HistoricalCertsRESTView, LastCertsRESTView
 
 urlpatterns = patterns('',
     # this URL passes resource_id in **kw to MyRESTView
     url(r'^heartbeat', HeartRESTView.as_view(), name='heart_rest_view'),
-    #url(r'^get_certs/last', 'asictapi.views.certs_download_yesterday', name = 'certs_download_yesterday'),
-    url(r'^get_certs/(?P<start_date>[^/]+)/(?P<end_date>[^/]+)', CertsRESTView.as_view(), name = 'certs_rest_view'),
+    url(r'^get_certs/last', LastCertsRESTView.as_view(), name = 'last_certs_download_yesterday'),
+    url(r'^get_certs/(?P<start_date>[^/]+)/(?P<end_date>[^/]+)', HistoricalCertsRESTView.as_view(), name = 'historical_certs_rest_view'),
 
 )
