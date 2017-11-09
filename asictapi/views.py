08a3fb6254f687580e33bed99f1b122e52429406
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-09 11:15:00 +0100

    test adding certsRESTview

--- asictapi/views.py
+++ asictapi/views.py
@@ -17,7 +17,7 @@ from rest_framework import status
 
 
 
-class MyRESTView(APIView):
+class HeartRESTView(APIView):
 
     def get(self, request, *args, **kw):
 
@@ -29,6 +29,33 @@ class MyRESTView(APIView):
 
         response = Response(risposta, status=status.HTTP_200_OK)
         return response
+
+class CertsRESTView(APIView):
+
+    def get(self, request, start_date, end_date):
+
+        logging.warning(request.user.get_username()) 
+        logging.warning(request.user.is_superuser)
+        
+        if not request.user.is_superuser:
+            raise PermissionDenied() 
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT SUBSTRING(uid,14) AS CODICE_PERSONA, AP.name AS NAME, course_id, CG.created_date AS DATA_CERTIFICATO \
+                from certificates_generatedcertificate CG \
+                JOIN auth_user A ON A.id = CG.user_id \
+                LEFT JOIN auth_userprofile AP ON A.id = AP.user_id \
+                LEFT JOIN social_auth_usersocialauth SAU ON A.id = SAU.user_id \
+                WHERE CG.status = 'downloadable' AND SUBSTRING(uid,14) IS NOT NULL \
+                AND provider != 'ecoopenid-auth' \
+                AND CONVERT(CG.created_date, datetime) >= CONVERT(%s, datetime) \
+                AND CONVERT(CG.created_date, datetime) <= CONVERT(%s, datetime)", 
+                (start_date, end_date))
+
+            result = cursor.fetchall()
+
+        return Response(result, safe=False)
 
 
 
