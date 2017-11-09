b8cd3db90c40745209c538bb222e05c50437f5d1
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-09 15:13:32 +0100

    query di test2

--- asictapi/views.py
+++ asictapi/views.py
@@ -54,16 +54,7 @@ class HistoricalCertsRESTView(APIView):
         #Query To DB
         with connection.cursor() as cursor:
             cursor.execute(
-                '''"SELECT SUBSTRING(uid,14) AS CODICE_PERSONA, AP.name AS NAME, course_id, CG.created_date AS DATA_CERTIFICATO \
-                from certificates_generatedcertificate CG \
-                JOIN auth_user A ON A.id = CG.user_id \
-                LEFT JOIN auth_userprofile AP ON A.id = AP.user_id \
-                LEFT JOIN social_auth_usersocialauth SAU ON A.id = SAU.user_id \
-                WHERE CG.status = 'downloadable' AND SUBSTRING(uid,14) IS NOT NULL \
-                AND provider != 'ecoopenid-auth' \
-                AND CONVERT(CG.created_date, datetime) >= CONVERT(%s, datetime) \
-                AND CONVERT(CG.created_date, datetime) <= CONVERT(%s, datetime)", 
-                (start_date, end_date))'''
+                
                 "SELECT * FROM student_courseenrollment where course_id='course-v1:Polimi+AL101+2017_M9' and is_active='1'")
 
 
@@ -81,6 +72,16 @@ class LastCertsRESTView(APIView):
 
 
 """
+'''"SELECT SUBSTRING(uid,14) AS CODICE_PERSONA, AP.name AS NAME, course_id, CG.created_date AS DATA_CERTIFICATO \
+                from certificates_generatedcertificate CG \
+                JOIN auth_user A ON A.id = CG.user_id \
+                LEFT JOIN auth_userprofile AP ON A.id = AP.user_id \
+                LEFT JOIN social_auth_usersocialauth SAU ON A.id = SAU.user_id \
+                WHERE CG.status = 'downloadable' AND SUBSTRING(uid,14) IS NOT NULL \
+                AND provider != 'ecoopenid-auth' \
+                AND CONVERT(CG.created_date, datetime) >= CONVERT(%s, datetime) \
+                AND CONVERT(CG.created_date, datetime) <= CONVERT(%s, datetime)", 
+                (start_date, end_date))'''
 
 def heartbeat(request):  # pylint: disable=unused-argument
     now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
