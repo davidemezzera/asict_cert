5efd356d5d1ac4fd906c0c358983c5c5da07d84d
Davide Mezzera <davide.mezzera@polimi.com>
2017-11-13 17:17:55 +0100

    fix token auth problem

--- asictapi/api.py
+++ asictapi/api.py
@@ -1,6 +1,7 @@
 from rest_framework.exceptions import PermissionDenied
 import logging
 
+from provider.oauth2 import models
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
 
